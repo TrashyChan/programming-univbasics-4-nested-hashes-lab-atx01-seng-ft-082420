3d9129029b05ea1e814d538190b583134de232e3
John Lonetree <jlonetree@gmail.com>
2020-08-09 22:30:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-  programmer_hash[:dennis_ritchie][:languages]%s
+  programmer_hash[:dennis_ritchie][:languages]%q
 end
 
 def adding_matz
