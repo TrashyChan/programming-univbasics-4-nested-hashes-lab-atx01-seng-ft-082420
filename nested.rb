5b1519e6b858322ce5437b5642e6cf3585ebcdc9
John Lonetree <jlonetree@gmail.com>
2020-08-09 22:38:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-  programmer_hash[:yukihiro_matsumoto][:languages] => ["Ruby"]
+  programmer_hash[:yukihiro_matsumoto][:languages] = ["Ruby"]
   programmer_hash[:yukihiro_matsumoto][:languages]
 end
 
